b8fcebe19710a23edca938d5708b42eee311a61b
Bartosz Piech <piech.bartosz696@gmail.com>
2022-10-14 10:46:06 +0200

    fixed indents

--- bprint_tests.c
+++ bprint_tests.c
@@ -7,58 +7,59 @@
 
 /* dog */
 typedef struct {
-    char *name;
-    char *race;
+	char *name;
+	char *race;
 } dog;
 
 void print_dog_old(void *d_ptr) {
-    dog *d = d_ptr;
-    printf("This is dog %s, he's %s.", d->name, d->race);
+	dog *d = d_ptr;
+	printf("This is dog %s, he's %s.", d->name, d->race);
 }
 
 void print_dog(dog *d) {
-    printf("This is dog %s, he's %s.", d->name, d->race);
+	printf("This is dog %s, he's %s.", d->name, d->race);
 }
 
 /* cat */
 typedef struct {
-    char *name;
-    int years;
+	char *name;
+	int years;
 } cat;
 
 void print_cat(void *c_ptr) {
-    cat *c = c_ptr;
-    printf("This is %s, he's %d years old", c->name, c->years);
+	cat *c = c_ptr;
+	printf("This is %s, he's %d years old", c->name, c->years);
 }
 
 /* point */
 typedef struct {
-    double x, y;
+	double x, y;
 } point;
 
 void print_point(point *ptr) {
-    printf("(%f, %f)", ptr->x, ptr->y);
+	printf("(%f, %f)", ptr->x, ptr->y);
 }
 
 int main() {
-    bprint_register("cat", print_cat);
-    bprint_register("point", print_point);
-    bprint_register("dog", print_dog);
+	bprint_register("cat", print_cat);
+	bprint_register("point", print_point);
+	bprint_register("dog", print_dog);
 
-    dog d = {
-        "Leon",
-        "Pomeranian",
-    };
-    cat c = {
-        "Andrew",
-        5,
-    };
-    cat c2 = {
-        "Baltazar",
-        23,
-    };
+	dog d = {
+		"Leon",
+		"Pomeranian",
+	};
+	cat c = {
+		"Andrew",
+		5,
+	};
+	cat c2 = {
+		"Baltazar",
+		23,
+	};
 
-    bprintln("These are my animals and my points: "bprint_bold bprint_under"{cat}, "bprint_reset"{cat}, {dog}, {point}", &c, &c2, &d, &(point) { 3.14, 2 });
-    bprintln(bprint_red_fg bprint_green_bg"Hello, "bprint_reset "World");
-    return 0;
+	bprintln("{dog} My dog is cute.", &d);
+	bprintln("These are my animals and my points: "bprint_bold bprint_under"{cat}, "bprint_reset"{cat}, {dog}, {point}", &c, &c2, &d, &(point) { 3.14, 2 });
+	bprintln(bprint_red_fg bprint_green_bg"Hello, "bprint_reset "World");
+	return 0;
 }
